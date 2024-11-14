1a9cf0b147a68360d460dc3755b4115a173a2bd3
fai7h01 <l.varsimashvil10@gmail.com>
2024-11-15 00:05:50 +0400

    controller

--- src/main/java/com/assistant/controller/ChatController.java
+++ src/main/java/com/assistant/controller/ChatController.java
@@ -1,16 +1,17 @@
 package com.assistant.controller;
 
+import com.assistant.dto.response.ResponseWrapper;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/assistant")
 public class ChatController {
@@ -22,13 +23,19 @@ public class ChatController {
     }
 
     @GetMapping
-    public String chat(@RequestParam(value = "ask") String userMessage) {
-        return chatClient.prompt()
+    public ResponseEntity<ResponseWrapper> chat(@RequestParam(value = "q") String userMessage) {
+        String content = chatClient.prompt()
                 .system(s -> s.param("current_date", LocalDate.now().toString()))
                 .user(userMessage)
                 .advisors(a -> a
                         .param(CHAT_MEMORY_CONVERSATION_ID_KEY, 1)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
                 .call().content();
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .code(HttpStatus.OK.value())
+                .success(true)
+                .message("Assistant response retrieved successfully.")
+                .data(content)
+                .build());
     }
 }
