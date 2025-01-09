47831468632377cd4d3d4542b8386a0dd173d5d8
fai7h01 <l.varsimashvil10@gmail.com>
2025-01-09 18:47:04 +0400

    dtos

---
+++ src/main/java/com/assistant/dto/Address.java
@@ -0,0 +1,26 @@
+package com.assistant.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Address {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
+}
---
+++ src/main/java/com/assistant/dto/User.java
@@ -0,0 +1,33 @@
+package com.assistant.dto;
+
+import com.assistant.enums.UserStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private LocalDate dateOfEmployment;
+    private boolean enabled;
+    private UserStatus userStatus = UserStatus.INACTIVE;
+    private Company company;
+    private Role role;
+
+
+}
